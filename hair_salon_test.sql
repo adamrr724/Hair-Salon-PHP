69d79d7dc244e02be0359a115f2aa900f5cb0292
Adam Ross Russell <adamrr724@yahoo.com>
2016-02-25 14:52:04 -0800

    add mysql database and addition of stylist_id to client class

---
+++ hair_salon_test.sql
@@ -0,0 +1,27 @@
+-- phpMyAdmin SQL Dump
+-- version 4.4.10
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Feb 25, 2016 at 11:49 PM
+-- Server version: 5.5.42
+-- PHP Version: 7.0.0
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `hair_salon_test`
+--
+CREATE DATABASE IF NOT EXISTS `hair_salon_test` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
+USE `hair_salon_test`;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
--- src/Client.php
+++ src/Client.php
@@ -4,13 +4,15 @@
         private $client_name;
         private $phone;
         private $email;
+        private $stylist_id;
         private $id;
 
-        function __construct($client_name, $phone, $email, $id = null)
+        function __construct($client_name, $phone, $email, $stylist_id, $id = null)
         {
             $this->client_name = $client_name;
             $this->phone = $phone;
             $this->email = $email;
+            $this->stylist_id = $stylist_id;
             $this->id = $id;
         }
 
@@ -29,6 +31,11 @@
             $this->email = $new_email;
         }
 
+        function setStylistId($new_stylist_id)
+        {
+            $this->stylist_id = $new_stylist_id;
+        }
+
         function getClientName()
         {
             return $this->client_name;
@@ -42,6 +49,11 @@
         function getEmail()
         {
             return $this->email;
+        }
+
+        function getStylistId()
+        {
+            return $this->stylist_id;
         }
 
         function getId()
