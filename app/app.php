1445a1e8a7a6ffc776da890b2734df6b30d75dc6
Adam Ross Russell <adamrr724@yahoo.com>
2016-02-26 11:34:36 -0800

    updated the roots to be accurate for the epicodus servers

--- app/app.php
+++ app/app.php
@@ -11,7 +11,7 @@
 
     $app['debug'] = true;
 
-    $server = 'mysql:host=localhost:8889;dbname=hair_salon';
+    $server = 'mysql:host=localhost;dbname=hair_salon';
     $username = 'root';
     $password = 'root';
     $DB = new PDO($server, $username, $password);
