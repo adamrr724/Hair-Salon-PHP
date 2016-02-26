884221676817d49c3d703ab1c0fdef486451d9a9
Adam Ross Russell <adamrr724@yahoo.com>
2016-02-26 08:41:51 -0800

    add route for adding/saving a stylist

--- app/app.php
+++ app/app.php
@@ -20,8 +20,20 @@
 
     $app->get("/", function() use ($app) {
         return $app['twig']->render('index.html.twig',
-        array(
-            'stylists' => Stylist::getAll()
+        array('stylists' => Stylist::getAll()));
+    });
+
+    $app->post("/stylist", function() use ($app) {
+        $new_stylist = new Stylist($_POST['name']);
+        $new_stylist->save();
+        return $app['twig']->render('index.html.twig', array('stylists' => Stylist::getAll()));
+    });
+
+    $app->get("/stylist/{id}", function($id) use ($app) {
+        $stylist = Stylist::find($id);
+        return $app['twig']->render('stylist.html.twig', array(
+            'stylist' => $stylist,
+            'clients' => $stylist->getClients()
         ));
     });
 
